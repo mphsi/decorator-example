c66098803216610f5e7b6997d762e603ad9462cd
Paul Hernández <mpaul.hernandez@gmail.com>
2021-04-21 23:06:44 -0500

    Add instructions on how to run a REPL with classes loaded

--- decorator_example.rb
+++ decorator_example.rb
@@ -1,17 +1,11 @@
 # frozen_string_literal: true
 
+# To load an IRB session with all classes do from root dir
+#   irb -I . -r decorator_example
 require_relative 'lib/beverage'
-require_relative 'lib/dark_roast'
 require_relative 'lib/house_blend'
-
-dark_roast = DarkRoast.new
-house_blend = HouseBlend.new
-
-puts "Price of a Dark Roast is: $#{dark_roast.cost}"
-puts "Price of a House Blend is: $#{house_blend.cost}"
-
-dark_roast_mocha = dark_roast
-house_blend_soy_steamed_milk = house_blend
-
-puts "Price of a Dark Roast, mocha is $#{dark_roast_mocha.cost}"
-puts "Price of a House Blend, soy, steamed milk is $#{house_blend_soy_steamed_milk.cost}"
+require_relative 'lib/dark_roast'
+require_relative 'lib/condiments/steamed_milk'
+require_relative 'lib/condiments/mocha'
+require_relative 'lib/condiments/soy'
+require_relative 'lib/condiments/whip'
